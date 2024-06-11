35cbb26d0ed0b3448d50bb2e07673263ff852850
dannyjameswilliams <fs19144@bristol.ac.uk>
2024-06-11 14:18:27 +0100

    initial_play.py follows quickstart tutorial

--- initial_play.py
+++ initial_play.py
@@ -1,14 +1,56 @@
 
 
-from api_key import key, url
+from keys import weaviate_key, weaviate_url, openai_key
 
 
 # connecting
 import weaviate
+import weaviate.classes as wvc
 import os
 
-with weaviate.connect_to_wcs(
-    cluster_url = url,  
-    auth_credentials = weaviate.auth.AuthApiKey(key)  
-) as client:  # Use this context manager to ensure the connection is closed
-    print(client.is_ready())
+import requests
+import json
+
+client = weaviate.connect_to_weaviate_cloud(
+    cluster_url = weaviate_url,
+    auth_credentials=weaviate.auth.AuthApiKey(weaviate_key),
+    headers={
+        "X-OpenAI-Api-Key": openai_key  # Replace with your inference API key
+    }
+)
+
+try:  
+
+    print(f"Client ready?: {client.is_ready()}")
+
+    if not client.collections.exists("Question"):
+        questions = client.collections.create(
+            name="Question",
+            vectorizer_config=wvc.config.Configure.Vectorizer.text2vec_openai(),  # If set to "none" you must always provide vectors yourself. Could be any other "text2vec-*" also.
+            generative_config=wvc.config.Configure.Generative.openai()  # Ensure the `generative-openai` module is used for generative queries
+        )
+
+    resp = requests.get('https://raw.githubusercontent.com/weaviate-tutorials/quickstart/main/data/jeopardy_tiny.json')
+    data = json.loads(resp.text)  # Load data
+
+    question_objs = list()
+    for i, d in enumerate(data):
+        question_objs.append({
+            "answer": d["Answer"],
+            "question": d["Question"],
+            "category": d["Category"],
+        })
+
+    questions = client.collections.get("Question")
+    questions.data.insert_many(question_objs)
+
+    response = questions.query.near_text(
+        query="biology",
+        limit=2
+    )
+
+    print(response.objects[0].properties)
+
+
+finally:
+    client.close()
